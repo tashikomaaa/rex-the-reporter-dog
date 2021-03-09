bb4fd4dcc2fb12ff6be0b750f9631f1db4769318
Aldwin <aldwin.moutarlier@b4finance.com>
2021-03-09 09:40:11 +0100

    refactor: add verbose

--- src/createRemoteComponent.tsx
+++ src/createRemoteComponent.tsx
@@ -29,22 +29,26 @@ export const createRemoteComponent = (
   dev?: boolean
 ): RemoteComponent => {
   const useRemoteComponent = createUseRemoteComponent(props);
-  if (dev) {
-    console.group(
-      "%cREX",
-      "background-color: #ffffff ; color: #ffffff ; font-weight: bold ; padding: 4px ;" +
-        "display: inline-block ; background-image: url( 'https://i.pinimg.com/originals/aa/21/8e/aa218e0d81d51178ab68f65ef759eb11.png' ) ; " +
-        "background-size: cover ; padding: 10px 175px 158px 10px ; " +
-        "border: 2px solid black ; font-size: 11px ; line-height: 11px ; "
-    );
-  }
+
   const remoteComponent: RemoteComponent = ({
     url,
     fallback = null,
     render,
+    dev,
     ...props
   }) => {
     const [loading, err, Component] = useRemoteComponent(url);
+    if (dev) {
+      console.group(
+        "%cREX",
+        "background-color: #ffffff ; color: #ffffff ; font-weight: bold ; padding: 4px ;" +
+          "display: inline-block ; background-image: url( 'https://i.pinimg.com/originals/aa/21/8e/aa218e0d81d51178ab68f65ef759eb11.png' ) ; " +
+          "background-size: cover ; padding: 10px 175px 158px 10px ; " +
+          "border: 2px solid black ; font-size: 11px ; line-height: 11px ; "
+      );
+      console.log("-- bones :");
+      console.log(props.url);
+    }
 
     if (loading) {
       return fallback;
