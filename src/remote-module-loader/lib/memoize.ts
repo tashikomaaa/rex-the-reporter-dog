bb0bd14e8b102d20e0d890ad706c32df361d9009
Aldwin <aldwin.moutarlier@b4finance.com>
2021-03-10 11:39:04 +0100

    inc remote module

---
+++ src/remote-module-loader/lib/memoize.ts
@@ -0,0 +1,18 @@
+/* eslint-disable */
+/**
+ * Memoizes a 1-arity function
+ *
+ * @param {Function} func Function to memoize
+ * @returns {Function} Memoized version of func.
+ */
+const memoize = func => {
+  const cache = {};
+  return key => {
+    if (key in cache == false) {
+      cache[key] = func(key);
+    }
+    return cache[key];
+  };
+};
+
+export default memoize;
