99d01c7c26c885cda543c0a19c9fe67b3b7620af
Aldwin <aldwin.moutarlier@b4finance.com>
2021-03-10 11:27:51 +0100

    add logo

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # rex-the-reporter-dog
 
-![alt text](https://cdn.britannica.com/60/8160-050-08CCEABC/German-shepherd.jpg)
+![alt text]()
 
 # Config
 
--- src/createRemoteComponent.tsx
+++ src/createRemoteComponent.tsx
@@ -41,7 +41,7 @@ export const createRemoteComponent = (
       console.group(
         "%cREX",
         "background-color: #ffffff ; color: #ffffff ; font-weight: bold ; padding: 4px ;" +
-          "display: inline-block ; background-image: url( 'https://i.pinimg.com/originals/aa/21/8e/aa218e0d81d51178ab68f65ef759eb11.png' ) ; " +
+          "display: inline-block ; background-image: url( './media/logo.png' ) ; " +
           "background-size: cover ; padding: 10px 175px 158px 10px ; " +
           "border: 2px solid black ; font-size: 11px ; line-height: 11px ; "
       );
